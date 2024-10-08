42aa6935a3c8fb3073762e8ff4fac0237bef1bfd
d-broder <daniel_sendreti@hotmail.com>
2024-10-08 11:49:55 -0300

    Ajuste a Navbar e inclua a fonte "Kumbh Sans"

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -1,36 +1,39 @@
+// app/home/layout.tsx (ou onde estiver sua Navbar)
 "use client";
-import Link from 'next/link';
+import Link from "next/link";
 
 const Navbar: React.FC = () => {
-  return (
-    <nav>
-      <ul>
-        <li>
-          <Link href="/">Home</Link>
-        </li>
-        <li>
-          <Link href="/home/dashboards">Dashboards</Link>
-        </li>
-        <li>
-          <Link href="/home/surveys">Surveys</Link>
-        </li>
-      </ul>
-      <style jsx>{`
-        nav {
-          padding: 1rem;
-          background-color: #333;
-        }
-        ul {
-          list-style: none;
-          display: flex;
-          gap: 1rem;
-        }
-        li {
-          color: white;
-        }
-      `}</style>
-    </nav>
-  );
+    return (
+        <div className="d-flex flex-column vh-100 text-white" style={{ width: "250px", backgroundColor: "#152259" }}>
+            <div className="text-center my-3">
+                <img src="/images/logo.png" alt="Logo" className="img-fluid mx-auto d-block" style={{ maxHeight: "100px" }} />
+                <h4 className="mt-2 mb-0 text-center">
+                    <strong>QUANTUM</strong>
+                </h4>
+                <p className="m-0 text-center">ENTERPRISE</p>
+            </div>
+
+            <nav className="flex-grow-1">
+                <ul className="nav flex-column">
+                    <li className="nav-item">
+                        <Link href="/" className="nav-link text-white">
+                            <i className="bi bi-house-door-fill me-2"></i> Home
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link href="/home/dashboards" className="nav-link text-white">
+                            <i className="bi bi-bar-chart-fill me-2"></i> Dashboards
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link href="/home/surveys" className="nav-link text-white">
+                            <i className="bi bi-journal-text me-2"></i> Surveys
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    );
 };
 
 export default Navbar;

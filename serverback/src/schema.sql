97a5952247e889542ddb5df1c3e983ba224524ec
gabrielvascf <93106982+gabrielvascf@users.noreply.github.com>
2024-10-30 09:35:05 -0300

    Atualizar esquema do banco de dados para incluir identificadores, datas de pesquisas, imagens de perfil, e mover campos JSON de pesquisas para colunas SQL

--- serverback/src/schema.sql
+++ serverback/src/schema.sql
@@ -7,7 +7,13 @@ CREATE TABLE `users` (
   `name` VARCHAR(255) NOT NULL,
   `email` VARCHAR(255) NOT NULL,
   `isAdmin` BOOLEAN DEFAULT false,
+  `phoneNumber` VARCHAR(255) NOT NULL,
   `password` VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE `userpictures` (
+  `user_id` INT NOT NULL,
+  `image` MEDIUMBLOB
 );
 
 CREATE TABLE `team` (
@@ -28,10 +34,11 @@ CREATE TABLE `team_member` (
 CREATE TABLE `survey` (
   `id` INT PRIMARY KEY AUTO_INCREMENT,
   `team_id` INT NOT NULL,
-  `data` JSON NOT NULL COMMENT 'Title: string,
-Description: string,
-Category: string
-',
+  `uid` VARCHAR(255) NOT NULL,
+  `title` VARCHAR(255) NOT NULL,
+  `description` TEXT(65535) NOT NULL,
+  `category` ENUM ('Autoavaliação', 'Avaliação de líder', 'Avaliação de liderado') NOT NULL,
+  `created` date NOT NULL,
   `questions` JSON NOT NULL COMMENT 'Question[]: {
   Type: ''Multiple'' | ''Single'' | ''Text'', 
   Title: string,
@@ -44,6 +51,7 @@ CREATE TABLE `survey_answer` (
   `answer_id` INT PRIMARY KEY AUTO_INCREMENT,
   `user_id` INT NOT NULL,
   `survey_id` INT NOT NULL,
+  `created` date NOT NULL,
   `data` JSON NOT NULL
 );
 
@@ -56,6 +64,8 @@ ALTER TABLE `team_leader` ADD FOREIGN KEY (`team_id`) REFERENCES `team` (`id`) O
 
 ALTER TABLE `team_leader` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 
+ALTER TABLE `userpictures` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+
 ALTER TABLE `team_member` ADD FOREIGN KEY (`team_id`) REFERENCES `team` (`id`) ON DELETE CASCADE;
 
 ALTER TABLE `team_member` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
@@ -63,7 +73,6 @@ ALTER TABLE `team_member` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) 
 ALTER TABLE `survey_answer` ADD FOREIGN KEY (`survey_id`) REFERENCES `survey` (`id`) ON DELETE CASCADE;
 
 ALTER TABLE `survey_answer` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+-- insere usuário padrão (daniel@gmail.com, 123456)
+--INSERT INTO users (name, email, password, phoneNumber, isAdmin) VALUES ('daniel', 'daniel@gmail.com','$2b$10$t2USd40dO76L.tsQSOo3WO75faZlQFC.CGDJISS.LJgufLd7ru/Hm', '12912345678', 1 ) 
 
---ao criar o schema no seu proprio pc adiconar essa linha
---INSERT INTO users (name, email, password, isAdmin) VALUES ('daniel', 'daniel@gmail.com','$2b$10$t2USd40dO76L.tsQSOo3WO75faZlQFC.CGDJISS.LJgufLd7ru/Hm', 1 ) 
---q cria um usuario como admin 

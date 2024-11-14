a8ab8a99095a19cb23f706131b3defcc7c1a8c45
Gabriecarvalho <gabrielcarvalho8@hotmail.com.br>
2024-11-14 10:00:19 -0300

    Correçao no tipo da pergunta

--- backend/src/types/Question.ts
+++ backend/src/types/Question.ts
@@ -1,6 +1,6 @@
 type Question = {
     question: string; // titulo
-    type: "Multiple" | "Single" | "Text"; 
+    type: "Multiple" | "Single(number)" | "Text" | "Single(text)"; // Tipo de pergunta
     options?: string[]; // Opções 
     category: string; // Categoria da pergunta
   };

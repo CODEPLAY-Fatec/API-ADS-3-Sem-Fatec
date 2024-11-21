fd4115c723467d50c3f39081c42ce6c9eada448e
ninabtolo <145863239+ninabtolo@users.noreply.github.com>
2024-11-21 15:13:01 -0300

    alteração layout responsivo

--- frontend/app/home/layout.tsx
+++ frontend/app/home/layout.tsx
@@ -1,10 +1,24 @@
+"use client";
+
 import Navbar from "@/components/Navbar";
+import { useState } from "react";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+    const [isExpanded, setIsExpanded] = useState(true);
+
     return (
         <div style={{ display: "flex" }}>
-            <Navbar />
-            <main style={{ flexGrow: 1, padding: "1rem", marginLeft: "250px" }}>{children}</main>
+            <Navbar onToggleSidebar={setIsExpanded} />
+            <main
+                style={{
+                    flexGrow: 1,
+                    marginLeft: isExpanded ? "255px" : "140px",
+                    padding: "1rem",
+                    transition: "margin-left 0.3s ease",
+                }}
+            >
+                {children}
+            </main>
         </div>
     );
 }

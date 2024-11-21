610bea73b7de055c70d7c52307a31c689386c91e
matmarquesx <matmarquesx@gmail.com>
2024-11-21 19:00:10 -0300

    Ajuste na logo navbar

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -233,7 +233,8 @@ const Navbar: React.FC<{ onToggleSidebar: (isExpanded: boolean) => void }> = ({ 
   <div className="d-flex justify-content-between align-items-center px-3 py-2">
     <div className="logo-section d-flex align-items-center">
       <Image src="/images/logo.png" alt="Logo" width={30} height={30} />
-      <h4 className="ms-2 m-0 text-white">QUANTUM</h4>
+      <h4 className="ms-2 m-0 text-white"><strong>QUANTUM</strong></h4>
+      <h4 className="ms-2 m-0 text-white">ENTERPRISE</h4>
     </div>
     <button className="btn btn-light btn-sm" onClick={toggleMenu}>
       ☰
